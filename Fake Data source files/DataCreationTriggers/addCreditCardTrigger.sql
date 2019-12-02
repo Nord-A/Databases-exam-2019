aac771a231cbd103055d8edd5b08bf0b3a9b0097
Jens Bäckvall <jensbackvall@t-nova.org>
2019-12-02 11:04:08 +0100

    add comments

--- Fake Data source files/DataCreationTriggers/addCreditCardTrigger.sql
+++ Fake Data source files/DataCreationTriggers/addCreditCardTrigger.sql
@@ -1,9 +1,13 @@
+-- This trigger will be discarded after adding one (1) credit card to each user
+
 CREATE TRIGGER addCreditCard
 ON TCreditCard
 AFTER INSERT AS
 BEGIN  
     UPDATE 
+        -- Set the current user ID on the card we are adding to be the same as the cards ID
         TCreditCard SET nUserId = nCreditCardId
+        -- 
         WHERE nCreditCardId = (SELECT TOP 1 nCreditCardId FROM TCreditCard
                                 ORDER BY nCreditCardId DESC);
     

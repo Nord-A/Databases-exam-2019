9fd77502da013f06e4960a11d1c2afa4d53f4fea
DjStonie <valdonay@yahoo.dk>
2019-12-02 11:13:56 +0100

    Update addCreditCardTrigger.sql

--- Fake Data source files/DataCreationTriggers/addCreditCardTrigger.sql
+++ Fake Data source files/DataCreationTriggers/addCreditCardTrigger.sql
@@ -7,14 +7,18 @@ BEGIN
     UPDATE 
         -- Set the current user ID on the card we are adding to be the same as the cards ID
         TCreditCard SET nUserId = nCreditCardId
-        -- 
+        -- Select the latest added creditcard by sorting on the auto incremented nCreditCardId and selecting top 1
         WHERE nCreditCardId = (SELECT TOP 1 nCreditCardId FROM TCreditCard
                                 ORDER BY nCreditCardId DESC);
     
     UPDATE 
+        -- Set the current cCardHolder on the card we are adding to be the same as the name and surname of the user 
+        -- with the UserId of the creditcard we added above
         TCreditCard SET cCardHolder = (SELECT CONCAT(cName, ' ' , cSurname) FROM TUser
+                                       -- Select the latest added creditcard by sorting on the auto incremented nCreditCardId and selecting top 1
                                         WHERE TUser.nUserId = (SELECT TOP 1 nCreditCardId FROM TCreditCard
                                                                 ORDER BY nCreditCardId DESC))
+        -- Select the latest added creditcard by sorting on the auto incremented nCreditCardId and selecting top 1
         WHERE nCreditCardId = (SELECT TOP 1 nCreditCardId FROM TCreditCard
                                 ORDER BY nCreditCardId DESC);
 END;
